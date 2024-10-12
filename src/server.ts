eab685099a219b5858f7188141baa88667376e1d
erdalgumuss <erdalgumus777@gmail.com>
2024-10-12 17:18:28 +0300

    cors install

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import express, { Application } from 'express';
+import cors from 'cors'; // CORS middleware'ini ekleyin
 import connectDB from './config/db';
 import adminRoutes from './routes/adminRoutes';
 import interviewRoutes from './routes/interviewRoutes';
-import questionRoutes from './routes/questionRoutes'; // Soru rotasını doğru şekilde ekleyin
+import questionRoutes from './routes/questionRoutes';
 import applicationRoutes from './routes/applicationRoutes';
-import errorHandler from './middlewares/errorHandlers'; // Hata yönetimi middleware'i
 import videoRoutes from './routes/videoRoutes';
+import errorHandler from './middlewares/errorHandlers'; // Hata yönetimi middleware'i
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
@@ -18,16 +18,15 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware'ler
+app.use(cors()); // CORS middleware'i ekliyoruz, isteklere izin veriyoruz
 app.use(express.json()); // JSON verisi işlemek için gerekli middleware
 
 // Rotalar
 app.use('/api/admin', adminRoutes);
 app.use('/api/interview', interviewRoutes);
-app.use('/api/question', questionRoutes); // /api/question yönlendirmesi
+app.use('/api/question', questionRoutes); 
 app.use('/api/application', applicationRoutes);
 app.use('/api/video', videoRoutes);
-
-
 
 // Genel hata yönetimi middleware'i
 app.use(errorHandler);
@@ -36,10 +35,3 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} numaralı portta çalışıyor.`);
 });
-app.use('/api/application', applicationRoutes);
-app.use('/api/video', videoRoutes);
-
-
-
-//    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MDhkOGIzZjU3NmYzOTcwMTk2NDAzMyIsImlhdCI6MTcyODYzOTAxOSwiZXhwIjoxNzI4NjQyNjE5fQ.zgO1zm74oM4L2wNQU-pcBi7ndDHBwyzbdPk6crlHpsQ",
-//    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MDhkOGIzZjU3NmYzOTcwMTk2NDAzMyIsImlhdCI6MTcyODY0MDg5OCwiZXhwIjoxNzI4NjQ0NDk4fQ.Vsl38bQelqT-hmZsKH0KvlAbe6F-SYfcnilFHJ52Uqw",
